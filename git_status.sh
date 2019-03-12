9311d674cfa87b381ea7f70347e4cf8a678e6c57
BrRoman <editeur@traditions-monastiques.com>
2019-03-12 15:02:22 +0100

    git_status updated (django-widget-tw., ordo-project).

--- git_status.sh
+++ git_status.sh
@@ -6,7 +6,7 @@
 echo
 echo '--------------------'
 echo
-echo 'absences :'
+echo 'absences:'
 cd "/media/fr_romain/DATA/Sites/absences/"
 git st
 git lg -10
@@ -14,7 +14,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'editor :'
+echo 'editor:'
 cd "/media/fr_romain/DATA/Editions/editor/"
 git st
 git lg -10
@@ -22,7 +22,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'liturgie :'
+echo 'liturgie:'
 cd "/media/fr_romain/DATA/Documents/Liturgie/liturgie/"
 git st
 git lg -10
@@ -30,7 +30,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'livrets :'
+echo 'livrets:'
 cd "/media/fr_romain/DATA/Sites/livrets/"
 git st
 git lg -10
@@ -38,7 +38,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'missa :'
+echo 'missa:'
 cd "/media/fr_romain/DATA/Sites/missa/"
 git st
 git lg -10
@@ -46,15 +46,23 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'ordomatic :'
-cd "/media/fr_romain/DATA/Sites/ordomatic/"
+echo 'ordomatic:'
+cd "/media/fr_romain/DATA/Sites/ordo-project/ordomatic/"
 git st
 git lg -10
 
 echo
 echo '--------------------'
 echo
-echo 'ornitho :'
+echo 'ordomatic-flav:'
+cd "/media/fr_romain/DATA/Sites/ordo-project/ordomatic-flav/"
+git st
+git lg -10
+
+echo
+echo '--------------------'
+echo
+echo 'ornitho:'
 cd "/media/fr_romain/DATA/Sites/ornitho/"
 git st
 git lg -10
@@ -73,7 +81,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'Compta :'
+echo 'compta:'
 cd "/media/fr_romain/DATA/Sites/compta/"
 git fetch upstream
 git st
@@ -82,7 +90,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'woodadmin :'
+echo 'woodadmin:'
 cd "/media/fr_romain/DATA/Sites/woodadmin/"
 git fetch upstream
 git st
@@ -94,7 +102,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'collatinus :'
+echo 'collatinus:'
 cd "/media/fr_romain/DATA/Documents/Langues/Latin/Collatinus/collatinus/"
 git fetch upstream
 git st
@@ -103,7 +111,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'divinum-officium :'
+echo 'divinum-officium:'
 cd "/media/fr_romain/DATA/Documents/Liturgie/divinum-officium/"
 git fetch upstream
 git st
@@ -112,7 +120,16 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'gregorio :'
+echo 'django-widget-tweaks:'
+cd "/home/fr_romain/.local/lib/python3.6/site-packages/django-widget-tweaks/"
+git fetch upstream
+git st
+git lg -10
+
+echo
+echo '--------------------'
+echo
+echo 'gregorio:'
 cd "/media/fr_romain/DATA/Documents/Informatique/Prog/Gregorio/gregorio/"
 git fetch upstream
 git st
@@ -121,7 +138,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'gregorio site :'
+echo 'gregorio site:'
 cd "/media/fr_romain/DATA/Documents/Informatique/Prog/Gregorio/gregorio-doc/"
 git fetch upstream
 git st
@@ -130,7 +147,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'jgabc :'
+echo 'jgabc:'
 cd "/media/fr_romain/DATA/Documents/Informatique/Prog/Gregorio/jgabc/"
 git fetch upstream
 git st
@@ -139,7 +156,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'latin-ecclesiastic-accents :'
+echo 'latin-ecclesiastic-accents:'
 cd "/media/fr_romain/DATA/Documents/Langues/Latin/latin-ecclesiastic-accents/"
 git fetch upstream
 git st
@@ -157,7 +174,7 @@ git lg -10
 echo
 echo '--------------------'
 echo
-echo 'zeal :'
+echo 'zeal:'
 cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/zeal/"
 git fetch upstream
 git st
