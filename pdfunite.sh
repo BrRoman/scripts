c5987bf1304b50449c8bbc88ecc6d44794b7ac65
BrRoman <editeur@traditions-monastiques.com>
2019-04-05 17:04:10 +0200

    Pdfunite: rm files after execution.

--- pdfunite.sh
+++ pdfunite.sh
@@ -3,3 +3,6 @@
 cd "/home/fr_romain/Desktop/"
 pdfunite 'Services - Semaines.pdf' 'Services - Oratoires.pdf' out.pdf
 evince out.pdf
+rm 'Services - Semaines.pdf'
+rm 'Services - Oratoires.pdf'
+rm 'out.pdf'
