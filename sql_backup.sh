9e97f9bd360bcb19058534b85867da55ec98f240
BrRoman <editeur@traditions-monastiques.com>
2019-08-16 10:36:50 +0200

    Sql_backup: formatting.

--- sql_backup.sh
+++ sql_backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # mysqldump all the databases:
+echo "> Dump local databases:"
 dumpmysql(){
     `mysqldump $1 > ~/Sites/sql/$1.sql`
     if [ "$?" -eq 0 ]; then
@@ -8,7 +9,6 @@ dumpmysql(){
     else
         echo "$1: problem dumping."
     fi
-    echo "---"
 }
 dumpmysql absences
 dumpmysql editor
@@ -18,14 +18,17 @@ dumpmysql missa
 dumpmysql ordomatic
 dumpmysql statistiques 
 
-
 # Download remote databases:
-echo "Download remote databases:"
-/home/fr_romain/Scripts/receive_sql_server.sh
 echo "---"
+echo ""
+echo "> Download remote databases:"
+/home/fr_romain/Scripts/receive_sql_server.sh
 
 
 # Update all the remote databases:
+echo "---"
+echo ""
+echo "> Update from remote databases:"
 mysql_update_from_remote(){
     `mysql $1 < ~/Sites/sql/$1_server.sql`
     if [ "$?" -eq 0 ]; then
@@ -33,10 +36,13 @@ mysql_update_from_remote(){
     else
         echo "$1: problem while updating from remote."
     fi
-    echo "---"
 }
 mysql_update_from_remote absences
 mysql_update_from_remote editor
 mysql_update_from_remote hotellerie 
 mysql_update_from_remote missa
 mysql_update_from_remote statistiques 
+
+
+echo "---"
+echo "END"
