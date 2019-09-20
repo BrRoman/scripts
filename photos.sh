d2f73efce1f4af3e7f9b1dee81a182b0f22f9619
BrRoman <editeur@traditions-monastiques.com>
2019-09-20 10:56:47 +0200

    Photos.sh: remove .xmp if no corresponding photo.

--- photos.sh
+++ photos.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 cd "/home/fr_romain/Pictures/"
-find . -name '*embedded_1.jpg' -o -name '*.jpg.xmp' | while read
+find . -name '*embedded_1.jpg' -o -name '*.jpg.xmp' | sort | while read
 do
     echo "$REPLY"
     rm "$REPLY"
+done
+
+# Remove .xmp files (Darktable) if no corresponding photo:
+find . -name '*.xmp' | sort | while read
+do
+    if [[ ! -f "${REPLY%.xmp}" ]]; then
+        echo "$REPLY"
+        rm "$REPLY"
+    fi
 done
