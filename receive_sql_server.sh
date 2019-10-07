5b9775354cb1831e907c6e4857e3c2ed419c682b
BrRoman <editeur@traditions-monastiques.com>
2019-10-07 16:51:56 +0200

    Receive_sql_server: option "r" included in "a".

--- receive_sql_server.sh
+++ receive_sql_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -arv fr_romain@192.168.1.21:/home/fr_romain/Sites/sql/*_server.sql /home/fr_romain/Sites/sql/
+rsync -av fr_romain@192.168.1.21:/home/fr_romain/Sites/sql/*_server.sql /home/fr_romain/Sites/sql/
