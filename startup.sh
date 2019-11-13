2e20a0aaad8828423c801815ca102bc12e29e4ba
BrRoman <editeur@traditions-monastiques.com>
2019-11-13 17:36:45 +0100

    Startup: cmd for "show hidden files" corr.

--- startup.sh
+++ startup.sh
@@ -7,10 +7,11 @@ thunderbird &
 code &
 
 while true; do
-# Counteract side effects of VSCode (as it seems):
-# 1. Set the key repeating's speed every second:
+# Counteract side effects of VSCode (as it seems) every second:
+# 1. Set the key repeating's speed:
 xset r rate 250 75;
-# 2. Set "show hidden files in Nautilus" to true every second:
-gsettings set org.gnome.nautilus.preferences show-hidden-files true;
+# 2. Set "show hidden files" to true:
+gsettings set org.gtk.Settings.FileChooser show-hidden true
+
 sleep 1;
 done
