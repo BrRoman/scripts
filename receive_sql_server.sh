8c23d7f1d8682c16c81788bd0441ab1e60b51880
BrRoman <editeur@traditions-monastiques.com>
2019-11-15 19:01:04 +0100

    Backup: rsync -arv again.

--- receive_sql_server.sh
+++ receive_sql_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -av fr_romain@192.168.1.21:/home/fr_romain/Sites/sql/*_server.sql /home/fr_romain/Sites/sql/
+rsync -arv fr_romain@192.168.1.21:/home/fr_romain/Sites/sql/*_server.sql /home/fr_romain/Sites/sql/
