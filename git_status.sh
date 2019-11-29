9b6c90315c7e36653dd8f75695f62f970905f40d
BrRoman <editeur@traditions-monastiques.com>
2019-11-29 08:25:10 +0100

    Git_status: "/DATA" rm; psalms added.

--- git_status.sh
+++ git_status.sh
@@ -7,7 +7,7 @@ echo
 echo '--------------------'
 echo
 echo 'absences:'
-cd "/media/fr_romain/DATA/Sites/absences/"
+cd "/media/fr_romain/Sites/absences/"
 git st
 git lg -10
 
@@ -15,7 +15,7 @@ echo
 echo '--------------------'
 echo
 echo 'accenteur:'
-cd "/media/fr_romain/DATA/Sites/accenteur/"
+cd "/media/fr_romain/Sites/accenteur/"
 git st
 git lg -10
 
@@ -23,7 +23,7 @@ echo
 echo '--------------------'
 echo
 echo 'editor:'
-cd "/media/fr_romain/DATA/Editions/editor/"
+cd "/media/fr_romain/Editions/editor/"
 git st
 git lg -10
 
@@ -31,7 +31,7 @@ echo
 echo '--------------------'
 echo
 echo 'hotellerie:'
-cd "/media/fr_romain/DATA/Sites/hotellerie/"
+cd "/media/fr_romain/Sites/hotellerie/"
 git st
 git lg -10
 
@@ -39,7 +39,7 @@ echo
 echo '--------------------'
 echo
 echo 'liturgie:'
-cd "/media/fr_romain/DATA/Documents/Liturgie/liturgie/"
+cd "/media/fr_romain/Documents/Liturgie/liturgie/"
 git st
 git lg -10
 
@@ -47,7 +47,7 @@ echo
 echo '--------------------'
 echo
 echo 'livrets:'
-cd "/media/fr_romain/DATA/Sites/livrets/"
+cd "/media/fr_romain/Sites/livrets/"
 git st
 git lg -10
 
@@ -55,7 +55,7 @@ echo
 echo '--------------------'
 echo
 echo 'missa:'
-cd "/media/fr_romain/DATA/Sites/missa/"
+cd "/media/fr_romain/Sites/missa/"
 git st
 git lg -10
 
@@ -63,7 +63,7 @@ echo
 echo '--------------------'
 echo
 echo 'ordo_flav:'
-cd "/media/fr_romain/DATA/Sites/ordo_flav/"
+cd "/media/fr_romain/Sites/ordo_flav/"
 git st
 git lg -10
 
@@ -71,7 +71,7 @@ echo
 echo '--------------------'
 echo
 echo 'ordomatic:'
-cd "/media/fr_romain/DATA/Sites/ordomatic/"
+cd "/media/fr_romain/Sites/ordomatic/"
 git st
 git lg -10
 
@@ -79,7 +79,15 @@ echo
 echo '--------------------'
 echo
 echo 'ornitho:'
-cd "/media/fr_romain/DATA/Sites/ornitho/"
+cd "/media/fr_romain/Sites/ornitho/"
+git st
+git lg -10
+
+echo
+echo '--------------------'
+echo
+echo 'psaumes:'
+cd "/home/fr_romain/Documents/Sainte_Ecriture/3 Ancien Testament/Cours sur les Psaumes/"
 git st
 git lg -10
 
@@ -95,7 +103,7 @@ echo
 echo '--------------------'
 echo
 echo 'statistiques:'
-cd "/media/fr_romain/DATA/Sites/statistiques/"
+cd "/media/fr_romain/Sites/statistiques/"
 git st
 git lg -10
 
@@ -103,7 +111,7 @@ echo
 echo '--------------------'
 echo
 echo 'typetrainer:'
-cd "/media/fr_romain/DATA/Sites/typetrainer/"
+cd "/media/fr_romain/Sites/typetrainer/"
 git st
 git lg -10
 
@@ -116,7 +124,7 @@ echo
 echo '--------------------'
 echo
 echo 'collatinus:'
-cd "/media/fr_romain/DATA/Documents/Langues/Latin/Collatinus/collatinus/"
+cd "/media/fr_romain/Documents/Langues/Latin/Collatinus/collatinus/"
 git fetch upstream
 git st
 git lg -10
@@ -125,7 +133,7 @@ echo
 echo '--------------------'
 echo
 echo 'divinum-officium:'
-cd "/media/fr_romain/DATA/Documents/Liturgie/divinum-officium/"
+cd "/media/fr_romain/Documents/Liturgie/divinum-officium/"
 git fetch upstream
 git st
 git lg -10
@@ -143,7 +151,7 @@ echo
 echo '--------------------'
 echo
 echo 'electronjs.org:'
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/electronjs.org/"
+cd "/media/fr_romain/Documents/Informatique/Prog/forks/electronjs.org/"
 git fetch upstream
 git st
 git lg -10
@@ -152,7 +160,7 @@ echo
 echo '--------------------'
 echo
 echo 'gregorio:'
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/Gregorio/gregorio/"
+cd "/media/fr_romain/Documents/Informatique/Prog/Gregorio/gregorio/"
 git fetch upstream
 git st
 git lg -10
@@ -161,7 +169,7 @@ echo
 echo '--------------------'
 echo
 echo 'gregorio site:'
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/Gregorio/gregorio-doc/"
+cd "/media/fr_romain/Documents/Informatique/Prog/Gregorio/gregorio-doc/"
 git fetch upstream
 git st
 git lg -10
@@ -170,7 +178,7 @@ echo
 echo '--------------------'
 echo
 echo 'jgabc:'
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/Gregorio/jgabc/"
+cd "/media/fr_romain/Documents/Informatique/Prog/Gregorio/jgabc/"
 git fetch upstream
 git st
 git lg -10
@@ -179,7 +187,7 @@ echo
 echo '--------------------'
 echo
 echo 'latin-ecclesiastic-accents:'
-cd "/media/fr_romain/DATA/Documents/Langues/Latin/latin-ecclesiastic-accents/"
+cd "/media/fr_romain/Documents/Langues/Latin/latin-ecclesiastic-accents/"
 git fetch upstream
 git st
 git lg -10
@@ -188,7 +196,7 @@ echo
 echo '--------------------'
 echo
 echo 'nodebook'
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/nodebook/"
+cd "/media/fr_romain/Documents/Informatique/Prog/forks/nodebook/"
 git fetch upstream
 git st
 git lg -10
@@ -197,7 +205,7 @@ echo
 echo '--------------------'
 echo
 echo 'nosqlclient'
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/nosqlclient/"
+cd "/media/fr_romain/Documents/Informatique/Prog/forks/nosqlclient/"
 git fetch upstream
 git st
 git lg -10
@@ -206,7 +214,7 @@ echo
 echo '--------------------'
 echo
 echo 'pencil'
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/pencil/"
+cd "/media/fr_romain/Documents/Informatique/Prog/forks/pencil/"
 git fetch upstream
 git st
 git lg -10
@@ -215,7 +223,7 @@ echo
 echo '--------------------'
 echo
 echo 'php-barcode:'
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/php-barcode"
+cd "/media/fr_romain/Documents/Informatique/Prog/forks/php-barcode"
 git fetch upstream
 git st
 git lg -10
@@ -224,7 +232,7 @@ echo
 echo '--------------------'
 echo
 echo 'summernote:'
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/summernote"
+cd "/media/fr_romain/Documents/Informatique/Prog/forks/summernote"
 git fetch upstream
 git st
 git lg -10
@@ -233,7 +241,7 @@ echo
 echo '--------------------'
 echo
 echo 'tcpdf:'
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/tcpdf"
+cd "/media/fr_romain/Documents/Informatique/Prog/forks/tcpdf"
 git fetch upstream
 git st
 git lg -10
@@ -242,7 +250,7 @@ echo
 echo '--------------------'
 echo
 echo 'zeal:'
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/zeal/"
+cd "/media/fr_romain/Documents/Informatique/Prog/forks/zeal/"
 git fetch upstream
 git st
 git lg -10
