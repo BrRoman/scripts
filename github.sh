139f2977fcc9ed1c33fafe43aa16eba398c4a8b1
BrRoman <editeur@traditions-monastiques.com>
2020-02-26 06:31:38 +0100

    Scripts server and github refreshed.

--- github.sh
+++ github.sh
@@ -31,15 +31,6 @@ git lg -10
 echo
 echo "--------------------"
 echo
-echo "bible_works:"
-cd "/media/fr_romain/DATA/Sites/bible_works/"
-git push origin
-git st
-git lg -10
-
-echo
-echo "--------------------"
-echo
 echo "editor:"
 cd "/media/fr_romain/DATA/Editions/editor/"
 git push origin
@@ -103,8 +94,8 @@ git lg -10
 echo
 echo "--------------------"
 echo
-echo "ordomatic_flavigny:"
-cd "/media/fr_romain/DATA/Sites/ordomatic_flavigny/"
+echo "ordomatic-flavigny:"
+cd "/media/fr_romain/DATA/Sites/ordomatic-flavigny/"
 git push origin
 git st
 git lg -10
@@ -171,15 +162,6 @@ git lg -10
 echo
 echo "--------------------"
 echo
-echo "collatinus:"
-cd "/media/fr_romain/DATA/Documents/Langues/Latin/Collatinus/collatinus/"
-git fetch upstream
-git st
-git lg -10
-
-echo
-echo "--------------------"
-echo
 echo "divinum-officium:"
 cd "/media/fr_romain/DATA/Documents/Liturgie/divinum-officium/"
 git fetch upstream
@@ -189,18 +171,9 @@ git lg -10
 echo
 echo "--------------------"
 echo
-echo "gregorio:"
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/Gregorio/gregorio/"
-git fetch upstream
-git st
-git lg -10
-
-echo
-echo "--------------------"
-echo
 echo "gregorio site:"
 cd "/media/fr_romain/DATA/Documents/Informatique/Prog/Gregorio/gregorio-doc/"
-git fetch upstream
+git fetch origin
 git st
 git lg -10
 
@@ -209,7 +182,7 @@ echo "--------------------"
 echo
 echo "jgabc:"
 cd "/media/fr_romain/DATA/Documents/Informatique/Prog/Gregorio/jgabc/"
-git fetch upstream
+git fetch origin 
 git st
 git lg -10
 
@@ -224,47 +197,3 @@ git push upstream
 git st
 git lg -10
 
-echo
-echo "--------------------"
-echo
-echo "pencil"
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/pencil/"
-git fetch upstream
-git st
-git lg -10
-
-echo
-echo "--------------------"
-echo
-echo "php-barcode:"
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/php-barcode"
-git fetch upstream
-git st
-git lg -10
-
-echo
-echo "--------------------"
-echo
-echo "summernote:"
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/summernote"
-git fetch upstream
-git st
-git lg -10
-
-echo
-echo "--------------------"
-echo
-echo "tcpdf:"
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/tcpdf"
-git fetch upstream
-git st
-git lg -10
-
-echo
-echo "--------------------"
-echo
-echo "zeal:"
-cd "/media/fr_romain/DATA/Documents/Informatique/Prog/forks/zeal/"
-git fetch upstream
-git st
-git lg -10
